49b4f8a61b2bd0900f23c2d9cec7888af6e20f0c
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-06-29 23:37:50 +0430

    ADD animations and styles to card loaders

--- src/Components/CardSkeleton/CardSkeleton.style.js
+++ src/Components/CardSkeleton/CardSkeleton.style.js
@@ -1,13 +1,30 @@
 import { createUseStyles } from "react-jss";
 
-createUseStyles({
+export default createUseStyles({
   root: {
     margin: "10px auto",
     "& > div": {
       padding: 15,
-      backgroundColor: "#eee",
+      backgroundColor: "#DDDBDD",
       borderRadius: 8,
       minHeight: 150,
+      position: "relative",
+      overflow: "hidden",
+      "&:after": {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        content: '""',
+        backgroundImage:
+          "linear-gradient(90deg,rgba(1,1,1, 0) 0,rgba(1,1,1, 0.2) 20%,rgba(1,1,1, 0.4) 60%,rgba(1,1,1, 0))",
+        transform: "translateX(-100%)",
+        animation: "$loading 2s infinite",
+      },
     },
+  },
+  "@keyframes loading": {
+    to: { transform: "translateX(100%)" },
   },
 });
