3d09570e8ab7e390323be96dabdc3e1542d49379
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-07-02 00:31:56 +0430

    ADD constants to theme provider

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { RecoilRoot } from "recoil";
 const theme = {
   borderColor: "#bcbcbc",
   textColor: "#333",
+  iconColor: "#fff",
   primaryColor: "#f95658",
+  boxShadowColor: "rgba(249, 86, 88,0.2)",
+  boxShadowStyle: "0px 0px 1px 5px rgba(249, 86, 88,0.2)",
 };
 
 const App = () => {
--- src/Components/TextInput/TextInput.jsx
+++ src/Components/TextInput/TextInput.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Styles from "./TextInput.style";
+import { useRecoilState } from "recoil";
+import { textInputState } from "../../Atoms/TextInput.atom";
 
 const TextInput = () => {
   const classes = Styles();
+  const [state, setState] = useRecoilState(textInputState);
   return (
     <div className={classes.root}>
-      <input value="همین جا بنویس" />
+      <input
+        onChange={(e) => setState(e.target.value)}
+        placeholder="همین جا بنویس"
+        value={state}
+      />
     </div>
   );
 };
