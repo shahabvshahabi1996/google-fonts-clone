5243617d94969a2bcf54078a8436a80b5911b051
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-07-02 00:30:30 +0430

    FIX Card Components css styles

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import Styles from "./Card.styles";
 import { useRecoilValue } from "recoil";
 import { sliderStateValue } from "../../Selectors/Card.selector";
+import { textInputValue } from "../../Selectors/TextContent.selector";
 import CardSkeleton from "../CardSkeleton";
 
 const Card = () => {
   const value = useRecoilValue(sliderStateValue);
   const classes = Styles({ fontSize: value });
   const [mounted, setMounted] = React.useState(false);
+  const textValue = useRecoilValue(textInputValue);
 
   useEffect(() => {
     setTimeout(() => {
@@ -18,7 +20,20 @@ const Card = () => {
   if (mounted) {
     return (
       <div className={classes.root}>
-        <div>Card</div>
+        <div className={classes.topBar}>
+          <h3>نام فونت</h3>
+          <div className={classes.iconContainer}>
+            <div>
+              <i className="material-icons">arrow_downward</i>
+            </div>
+            <div>
+              <i className="material-icons">bookmark</i>
+            </div>
+          </div>
+        </div>
+        <div contentEditable className={classes.content}>
+          {textValue ? textValue : "این یک نوشته است"}
+        </div>
       </div>
     );
   }
--- src/Components/Card/Card.styles.js
+++ src/Components/Card/Card.styles.js
@@ -1,17 +1,49 @@
 import { createUseStyles } from "react-jss";
 
-export default createUseStyles({
+export default createUseStyles((theme) => ({
   root: {
     margin: "10px auto",
-    "& > div": {
-      fontSize: ({ fontSize }) => {
-        console.log(fontSize);
-        return fontSize;
-      },
-      padding: 15,
-      // backgroundColor: "#eee",
-      borderTop: "1px solid #ddd",
-      minHeight: 150,
+    minHeight: 150,
+    position: "relative",
+  },
+  content: {
+    fontSize: ({ fontSize }) => {
+      console.log(fontSize);
+      return fontSize;
+    },
+    padding: 15,
+  },
+  topBar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottom: "1px solid #ddd",
+    "& > h3": {
+      fontWeight: 300,
+      fontSize: 20,
     },
   },
-});
+  iconContainer: {
+    display: "flex",
+    alignItems: "flex-end",
+    "& > div": {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      width: 30,
+      height: 30,
+      borderRadius: "50%",
+      backgroundColor: `${theme.primaryColor}`,
+      marginLeft: 5,
+      color: `${theme.iconColor}`,
+      cursor: "pointer",
+      transition: "box-shadow 0.3s",
+      "& > i": {
+        fontSize: 20,
+      },
+      "&:hover": {
+        boxShadow: `${theme.boxShadowStyle}`,
+      },
+    },
+  },
+}));
