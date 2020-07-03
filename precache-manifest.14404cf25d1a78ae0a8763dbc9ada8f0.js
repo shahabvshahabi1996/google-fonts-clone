47f1c0feca8ca52acecf6253056d83e195643f32
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-07-04 01:09:31 +0430

    Updates

---
+++ precache-manifest.14404cf25d1a78ae0a8763dbc9ada8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f2cc81ec73671fa1f9db64c616e942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123abb2d1c47356eea21",
+    "url": "/static/css/main.1577ad88.chunk.css"
+  },
+  {
+    "revision": "a40e8b3effa0f71e5424",
+    "url": "/static/js/2.dcb6b3d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dcb6b3d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123abb2d1c47356eea21",
+    "url": "/static/js/main.0c66571a.chunk.js"
+  },
+  {
+    "revision": "a66d8bff7464d579e54a",
+    "url": "/static/js/runtime-main.9d39134a.js"
+  }
+]);
