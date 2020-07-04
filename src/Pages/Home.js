7aed38f908e4a4bd9836a696d64683e852440573
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-07-04 11:38:08 +0430

    ADD Skeleton card to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,8 @@ import Layout from "../Components/Layout";
 import Card from "../Components/Card";
 import { createUseStyles } from "react-jss";
 import { CustomThemeContext } from "../Components/CustomThemeProvider/";
+import CardSkeleton from "../Components/CardSkeleton/CardSkeleton";
+import LazyLoad from "react-lazyload";
 
 const styles = createUseStyles({
   parent: {
@@ -37,12 +39,14 @@ const Home = () => {
   const classes = styles({ containerStyle });
   const [fonts, setFonts] = React.useState([]);
   const [sentences, setSentences] = React.useState({});
+  const [mounted, setMounted] = React.useState(false);
 
-  const fetchFonts = () => {
+  const fetchFonts = (resolve) => {
     fetch("https://api.fontgraphy.ir/metadata/fonts")
       .then((res) => res.json())
       .then((result) => {
         setFonts(result.fonts);
+        resolve(true);
       });
   };
 
@@ -58,29 +62,70 @@ const Home = () => {
             };
           }, {})
         );
+        setMounted(true);
       });
   };
 
   useEffect(() => {
-    fetchFonts();
-    fetchSentenses();
+    // eslint-disable-next-line no-undef
+    new Promise((resolve) => {
+      fetchFonts(resolve);
+    }).then((res) => {
+      if (res) {
+        fetchSentenses();
+      }
+    });
   }, []);
 
   return (
     <Layout>
       <div className={classes.parent}>
-        {fonts.map((font, index) => (
-          <div key={font._id} className={classes.child}>
-            <Card
-              content={
-                sentences[index % Object.keys(sentences).length] ||
-                "یک نوشته زیبا"
-              }
-              familyInPersian={font.familyInPersian}
-              family={font.family}
-            />
-          </div>
-        ))}
+        {mounted ? (
+          fonts.map((font, index) => (
+            <div key={font._id} className={classes.child}>
+              <LazyLoad height={250} offset={100}>
+                <Card
+                  content={
+                    sentences[index % Object.keys(sentences).length] ||
+                    "یک نوشته زیبا"
+                  }
+                  familyInPersian={font.familyInPersian}
+                  family={font.family}
+                />
+              </LazyLoad>
+            </div>
+          ))
+        ) : (
+          <>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+            <div className={classes.child}>
+              <CardSkeleton />
+            </div>
+          </>
+        )}
       </div>
     </Layout>
   );
