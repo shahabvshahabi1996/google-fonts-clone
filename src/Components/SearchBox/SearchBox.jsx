1fb9b739da5d077762bcc3f839e3218d301d1f62
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-07-05 22:22:57 +0430

    ADD recoil to SearchBox

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Styless from "./SearchBox.style";
+import { useRecoilState } from "recoil";
+import { searchInputState } from "../../Atoms/SearchInput.atom";
 
 const SearchBox = () => {
   const classes = Styless();
+  const [value, setValue] = useRecoilState(searchInputState);
   return (
     <div className={classes.root}>
       <i className="material-icons">search</i>
-      <input placeholder="هرچی دل تنگت میخواهد سرچ کن.." />
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="هر آن چیز که در جستن آنی , آنی..."
+      />
     </div>
   );
 };
