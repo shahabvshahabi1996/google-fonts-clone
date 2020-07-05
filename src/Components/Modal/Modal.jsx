edb68ea7480f4dbcd978befc275f80b919dc36dd
Alireza Shahabi <shahabvshahabi1996@gmail.com>
2020-07-05 22:23:46 +0430

    ADD Modal Component to project

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import Styles from "./Modal.style";
+import PropType from "prop-types";
+
+const Modal = ({ familyInPersian = "", family = "", closeModal }) => {
+  const [isModalClosing, setModalClosing] = React.useState(false);
+  const classes = Styles();
+  const closingModal = () => {
+    setModalClosing(true);
+    setTimeout(() => {
+      closeModal();
+    }, 300);
+  };
+
+  return createPortal(
+    <div
+      onClick={() => closingModal()}
+      className={`${classes.root} ${isModalClosing ? classes.closingRoot : ""}`}
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`${classes.content} ${
+          isModalClosing ? classes.closingContent : ""
+        }`}
+      >
+        <div onClick={() => closingModal()} className={classes.closeIcon}>
+          <i className="material-icons">close</i>
+        </div>
+        <div className={classes.contentHeader}>
+          <h2>
+            دانلود فونت{" "}
+            <span className={classes.fontName}>{familyInPersian}</span>
+          </h2>
+          {/* <div className={classes.iconButton}>
+            <i className="material-icons">bookmark_outlined</i>
+          </div> */}
+        </div>
+        <p>
+          درصورتی که میخواهید فونت انتخاب شده رو ما براتون سرو کنیم و از سرور ما
+          دریافت کنید، تنها کافیه که کد زیر را در تگ head صفحه html خود قرار
+          دهید
+        </p>
+        <div className={classes.codeBox}>
+          <code>
+            {`<link
+                  rel="stylesheet"
+                  href="https://api.fontgraphy.ir/css?family=${family}"></link>`}
+          </code>
+        </div>
+        <hr />
+        <h3>استفاده در CSS</h3>
+        <p>برای استفاده از فونت باید از کد css زیر استفاده کنید</p>
+        <div className={classes.codeBox}>
+          <code>{`font-family: ${family}, sans-serif;`}</code>
+        </div>
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+Modal.propTypes = {
+  family: PropType.string.isReuired,
+  familyInPersian: PropType.string.isRequired,
+  closeModal: PropType.func.isRequired,
+};
+
+export default Modal;
---
+++ src/Components/Modal/Modal.style.js
@@ -0,0 +1,130 @@
+import { createUseStyles } from "react-jss";
+export default createUseStyles((theme) => ({
+  root: {
+    backgroundColor: "rgba(0,0,0,0.37)",
+    width: "100vw",
+    height: "100vh",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    zIndex: 3,
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "flex-start",
+    animation: "$fading 0.3s 1",
+  },
+  closingRoot: {
+    animation: "$hiding 0.3s 1 !important",
+  },
+
+  closingContent: {
+    animation: "$end 0.3s 1 !important",
+  },
+
+  content: {
+    position: "relative",
+    width: "60%",
+    minHeight: 450,
+    margin: "0 auto",
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: "30px 50px 0 50px",
+    animation: "$start 0.3s 1",
+    color: "#717171",
+    boxShadow: "0px 5px 5px #333",
+    "& p": {
+      fontSize: 16,
+    },
+    "@media screen and (max-width:720px)": {
+      width: "95vw",
+      padding: "40px 15px 0 15px",
+    },
+  },
+  contentHeader: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  iconButton: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 30,
+    height: 30,
+    borderRadius: "50%",
+    cursor: "pointer",
+    backgroundColor: `${theme.primaryColor}`,
+    verticalAlign: "middle",
+    transition: "box-shadow 0.3s",
+    color: "#fff",
+    backgroundSize: "contain",
+    "& > i": {
+      fontSize: 20,
+      display: "inline-flex",
+      verticalAlign: "middle",
+    },
+    "&:hover": {
+      boxShadow: `${theme.boxShadowStyle}`,
+    },
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 8,
+    right: 8,
+    backgroundColor: "#eee",
+    padding: 10,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "50%",
+    cursor: "pointer",
+  },
+  fontName: {
+    color: "#4a4a4a",
+  },
+  codeBox: {
+    backgroundColor: "#eee",
+    padding: "20px 25px",
+    direction: "ltr",
+    "& > code": {
+      wordBreak: "break-word",
+    },
+  },
+  "@keyframes start": {
+    from: {
+      transform: "translateY(100%)",
+    },
+    to: {
+      transform: "translateY(0%)",
+    },
+  },
+
+  "@keyframes end": {
+    from: {
+      transform: "translateY(0%)",
+    },
+    to: {
+      transform: "translateY(100%)",
+    },
+  },
+
+  "@keyframes fading": {
+    from: {
+      opacity: 0,
+    },
+    to: {
+      opacity: 1,
+    },
+  },
+
+  "@keyframes hiding": {
+    from: {
+      opacity: 1,
+    },
+    to: {
+      opacity: 0,
+    },
+  },
+}));
